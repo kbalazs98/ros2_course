2515ae54abfa1c13df8daceeff825ef3c6f389d4
kbalazs98 <karmi.balazs@wstud.uni-obuda.hu>
2023-05-11 19:37:33 +0200

    jon megy, neha sokat forog korbe korbe

--- ros2_course/turtlebot_controller.py
+++ ros2_course/turtlebot_controller.py
@@ -6,7 +6,6 @@ import random
 import math
 
 
-
 class Turtlebot_Controller(Node):
     def __init__(self):
         print("turtlebot controller vagyok")
@@ -25,36 +24,48 @@ class Turtlebot_Controller(Node):
         ranges = scan_data.ranges
         #for range in enumerate(ranges):
         print(ranges[0])
-        if(ranges[0]<0.5):
-                
+        for i in range(360):
+            if((0<=i and i<=20) or (339<=i and i<=359)):   
+                if(ranges[i]<0.55):
+
                 #obstacle_angle = scan_data.angle_min + range_idx * scan_data.angle_increment
                 #opposite_angle = math.atan2(math.sin(obstacle_angle),math.cos(obstacle_angle)+math.pi)
                 #self.cmd_vel_msg.linear.x = -1.0
                 #self.cmd_vel_msg.angular.z = opposite_angle
                 #self.publisher.publish(self.cmd_vel_msg)
-            self.turn(30.0,180.0)
-        print("nem fal")
-        self.cmd_vel_msg.linear.x = 0.2
+                    print("szog",i)
+                    print("tavolsaga",ranges[i])
+                    print("FORDULKEZDODES")
+                    angle = i+180
+                    print(angle)
+                    if(angle>360):
+                        self.turn(omega = 30.0, angle = angle-360+random.uniform(-15,15))
+                    else:
+                        self.turn(omega = 30.0, angle = angle+random.uniform(-15,15))
+                    return
+        #print("nem fal")
+        self.cmd_vel_msg.linear.x = 0.4
         self.cmd_vel_msg.angular.z = 0.0
         self.publisher.publish(self.cmd_vel_msg)
 
     def turn(self, omega, angle):
-        
         self.cmd_vel_msg.linear.x = 0.0
         self.cmd_vel_msg.angular.z = math.radians(omega)
         self.publisher.publish(self.cmd_vel_msg)
 
-        rate = self.create_rate(10)  # Adjust the publishing frequency as needed
-        
-        T= abs(angle/omega)
+        #rate = self.create_rate(100)  # Adjust the publishing frequency as needed
 
+        T = abs(angle / omega)
         when = self.get_clock().now() + rclpy.time.Duration(seconds = T)
 
-        while(self.get_clock().now() < when) and rclpy.ok():
-            print("porog")      
-        
-            #rclpy.spin_once(self)
-        print("befejeztem a forgast")
+        while (self.get_clock().now() < when) and rclpy.ok():
+            self.publisher.publish(self.cmd_vel_msg)
+            #print(when-self.get_clock().now())
+            #rate.sleep()
+
+        self.cmd_vel_msg.angular.z = 0.0
+        self.publisher.publish(self.cmd_vel_msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
