070fcc0bef42c3ad233594fb437ab82af1e3f192
kbalazs98 <karmi.balazs@wstud.uni-obuda.hu>
2023-05-11 15:03:10 +0200

    turtlebot goes forward until reaching obstacle

--- ros2_course/turtlebot_controller.py
+++ ros2_course/turtlebot_controller.py
@@ -1,6 +1,7 @@
-from rclpy.node import Node
 import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 
 
@@ -9,25 +10,32 @@ class Turtlebot_Controller(Node):
         print("turtlebot controller vagyok")
         super().__init__('turtlebot_controller')
 
-        node = rclpy.create_node('lidar_subscriber')
+        #node = rclpy.create_node('lidar_subscriber')
 
-        subscriber = node.create_subscription(LaserScan,'/scan',scan_callback,10)
-        subscriber
+        self.subscription = self.create_subscription(LaserScan,'/scan',self.scan_callback,10)
+        self.publisher = self.create_publisher(Twist,'/cmd_vel',10)
+        self.cmd_vel_msg = Twist()
 
-        try:
-            rclpy.spin(node)
-        except KeyboardInterrupt:
-            pass
-
-def scan_callback(scan_data):
-    ranges = scan_data.ranges
-    if len(ranges)>0:
-        print("Distance to the nearest obstacle: ", ranges[0])
-
+    def scan_callback(self,scan_data):
+        ranges = scan_data.ranges
+        for range in ranges:
+            if range < 0.2:
+                print("fal")
+                print(range)
+                self.cmd_vel_msg.linear.x = 0.0
+                self.cmd_vel_msg.angular.z = 0.0
+                self.publisher.publish(self.cmd_vel_msg)
+                return
+            
+        print("nem fal")
+        self.cmd_vel_msg.linear.x = 0.2
+        self.cmd_vel_msg.angular.z = 0.0
+        self.publisher.publish(self.cmd_vel_msg)
+    
 def main(args=None):
     rclpy.init(args=args)
     tbc = Turtlebot_Controller()
-
+    rclpy.spin(tbc)
 
 
     tbc.destroy_node()
