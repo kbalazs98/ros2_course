220cf8b9f73cb8cde362cb527730132fd4203981
kbalazs98 <karmi.balazs@wstud.uni-obuda.hu>
2023-05-12 19:31:38 +0200

    teljes logic atszervezes, most mar becsuletesen porszivozik

--- ros2_course/turtlebot_controller.py
+++ ros2_course/turtlebot_controller.py
@@ -5,74 +5,72 @@ from geometry_msgs.msg import Twist
 import random
 import math
 
-
 class Turtlebot_Controller(Node):
     def __init__(self):
-        print("turtlebot controller vagyok")
-        super().__init__('turtlebot_controller')
 
-        #node = rclpy.create_node('lidar_subscriber')
+        super().__init__('turtlebot_controller')
 
         self.subscription = self.create_subscription(LaserScan,'/scan',self.scan_callback,10)
         self.publisher = self.create_publisher(Twist,'/cmd_vel',10)
+        self.ranges = []
         self.cmd_vel_msg = Twist()
 
-
-       
-
     def scan_callback(self,scan_data):
-        ranges = scan_data.ranges
-        #for range in enumerate(ranges):
-        print(ranges[0])
-        for i in range(360):
-            if((0<=i and i<=20) or (339<=i and i<=359)):   
-                if(ranges[i]<0.55):
+        self.ranges = scan_data.ranges
+        #print("tavolsag: ",self.ranges[0])
+        self.go_forward()
 
-                #obstacle_angle = scan_data.angle_min + range_idx * scan_data.angle_increment
-                #opposite_angle = math.atan2(math.sin(obstacle_angle),math.cos(obstacle_angle)+math.pi)
-                #self.cmd_vel_msg.linear.x = -1.0
-                #self.cmd_vel_msg.angular.z = opposite_angle
-                #self.publisher.publish(self.cmd_vel_msg)
-                    print("szog",i)
-                    print("tavolsaga",ranges[i])
-                    print("FORDULKEZDODES")
-                    angle = i+180
-                    print(angle)
-                    if(angle>360):
-                        self.turn(omega = 30.0, angle = angle-360+random.uniform(-15,15))
-                    else:
-                        self.turn(omega = 30.0, angle = angle+random.uniform(-15,15))
-                    return
-        #print("nem fal")
-        self.cmd_vel_msg.linear.x = 0.4
-        self.cmd_vel_msg.angular.z = 0.0
+    def go_forward(self):
+        self.cmd_vel_msg.linear.x = 0.25
         self.publisher.publish(self.cmd_vel_msg)
-
-    def turn(self, omega, angle):
+        try:
+            angle = self.calc()
+            if(angle != 0):
+                self.turn(35,angle)
+        except Exception as ex:
+            print(ex)
+            
+    def turn(self,omega,angle):
         self.cmd_vel_msg.linear.x = 0.0
-        self.cmd_vel_msg.angular.z = math.radians(omega)
+        self.cmd_vel_msg.angular.z = -math.radians(omega)
         self.publisher.publish(self.cmd_vel_msg)
 
-        #rate = self.create_rate(100)  # Adjust the publishing frequency as needed
+        self.create_rate(100)
 
         T = abs(angle / omega)
         when = self.get_clock().now() + rclpy.time.Duration(seconds = T)
 
+        print(self.cmd_vel_msg.angular.z )
+        print(T)
+
         while (self.get_clock().now() < when) and rclpy.ok():
             self.publisher.publish(self.cmd_vel_msg)
-            #print(when-self.get_clock().now())
-            #rate.sleep()
-
+            
+        print("vege")
         self.cmd_vel_msg.angular.z = 0.0
         self.publisher.publish(self.cmd_vel_msg)
 
+    def calc(self):
+        for i in range(360):
+            if((0<=i and i<=25) or (334<=i and i<=359)):   
+                if(self.ranges[i]<0.42):
+                    print("akadaly iranya: ",i)
+                    print("tavolsaga: ",self.ranges[i])
+                    angle = i+180
+                    if(angle>360):
+                        angle = angle-360+random.uniform(-40,40) 
+                    else:
+                        angle = angle+random.uniform(-40,40)
+                    return angle
+        return 0     
 
 def main(args=None):
     rclpy.init(args=args)
     tbc = Turtlebot_Controller()
+
     rclpy.spin(tbc)
-
-
+    
+    
     tbc.destroy_node()
     rclpy.shutdown()
 
